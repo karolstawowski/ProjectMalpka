ea29143751746de70c3cb3e914e2dc46c187c67a
Karol Stawowski <66377130+karolstawowski@users.noreply.github.com>
2022-01-29 20:07:51 +0100

    fix: fix a total failure...

--- app.js
+++ app.js
@@ -1,120 +1,120 @@
-//Połączenie z botem
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-//Praca z plikiem zewnętrznym
-const fs = require('fs');
+const fs = require("fs");
 
-//Odczytywanie hasła dostępu do bota
-fs.readFile('password.txt', function (err, data) {
-    if (err) {
-      throw err; 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+fs.readFile("password.txt", function (err, data) {
+  if (err) {
+    throw err;
+  }
+  const token = data.toString();
+
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+
+  client.on("message", (msg) => {
+    if (msg.mentions.has(client.user)) {
+      const description =
+        "!sing - singing_skul.mp4\n" +
+        "!skul - skul.jpg\n" +
+        "!happyskul - happy_skul.png\n" +
+        "!kreciolek - kreciolek.gif\n" +
+        "skul. - loop of skuls\n" +
+        "karol top - gra, w którą Karol dziś nie zagra\n" +
+        "karol top sql - gra, w którą Karol dziś nie zagra - SQL Edition";
+
+      const embed = new Discord.MessageEmbed()
+        .setTitle("Pomoc dla Serwerowego Pomocnika")
+        .setColor(0x0900ab)
+        .setDescription(description);
+
+      msg.channel.send(embed);
     }
-    const token = data.toString();
+    if (msg.content.toLowerCase() === "!sing") {
+      const attachment = new Discord.MessageAttachment(
+        "./assets/singing_skul.mp4"
+      );
+      msg.channel.send(attachment);
+    }
+    if (msg.content.toLowerCase() === "!skul") {
+      const attachment = new Discord.MessageAttachment("./assets/fat_skul.jpg");
+      msg.channel.send(attachment);
+    }
+    if (msg.content.toLowerCase() === "!happyskul") {
+      const attachment = new Discord.MessageAttachment(
+        "./assets/happy_skul.png"
+      );
+      msg.channel.send(attachment);
+    }
+    if (
+      (msg.content.toLowerCase() === "!kreciolek") |
+      (msg.content.toLowerCase() === "!kręciołek")
+    ) {
+      const attachment = new Discord.MessageAttachment("./assets/skul.gif");
+      msg.channel.send(attachment);
+    }
+    if (msg.content.toLowerCase() === "skul.") {
+      (async () => {
+        const attachment = new Discord.MessageAttachment(
+          "./assets/fat_skul.jpg"
+        );
+        for (var i = 0; i < 20; i++) {
+          msg.channel.send(attachment);
+          await new Promise((resolve) => setTimeout(resolve, 5000));
+        }
+      })();
+    }
+    if (msg.content.toLowerCase() === "karol top") {
+      const randomNumber = getRandomIntInclusive(0, 113038);
 
-    client.on('ready', () => {
-        console.log(`Logged in as ${client.user.tag}!`);
-    });
-
-    //Funkcje bota
-    client.on('message', msg => {
-        if (msg.content.toLowerCase() === '!malpka' | msg.content.toLowerCase() === '!małpka')
-        {
-            const opis = '!małpka - pomoc\n'+
-            '!hepiskul - hepiskul.png\n'+
-            '!grubyskul - grubyskul.png\n'+
-            '!ogromnyskul - ogromnyskul.jpg\n'+
-            '!kreciolek - kreciolek.gif\n'+
-            'skul. - loop of skuls\n'
-
-            const embed = new Discord.MessageEmbed()
-            .setTitle('Pomoc dla Serwerowego Skulwysyna')
-            .setColor(0x0900AB)
+      fs.readFile(
+        "./assets/api.steampowered.com.json",
+        "utf8",
+        function (err, data) {
+          if (err) throw err;
+          const obj = JSON.parse(data);
+          const opis = obj.applist.apps[randomNumber].name;
+          const embed = new Discord.MessageEmbed()
+            .setTitle("Gra, w którą Karol nie zagra: ")
+            .setColor(0x0900ab)
             .setDescription(opis);
 
+          msg.channel.send(embed);
+        }
+      );
+    }
+    if (msg.content.toLowerCase() === "karol top sql") {
+      const randomNumber = getRandomIntInclusive(0, 113038);
+
+      var mysql = require("mysql");
+      var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        database: "steam_games",
+      });
+
+      con.connect(function (err) {
+        if (err) throw err;
+        con.query(
+          "SELECT GameName FROM Games WHERE `ID` = " + randomNumber + ";",
+          (err, results, fields) => {
+            if (err) throw err;
+            const embed = new Discord.MessageEmbed()
+              .setTitle("Gra, w którą Karol nie zagra: ")
+              .setColor(0x0900ab)
+              .setDescription(results[0].GameName);
             msg.channel.send(embed);
-        }
-        if (msg.content.toLowerCase() === '!hepiskul') 
-        {
-            const attachment = new Discord.MessageAttachment('happyskul.png');
-            msg.channel.send(attachment);
-        }
-        if (msg.content.toLowerCase() === '!grubyskul') 
-        {
-            const attachment = new Discord.MessageAttachment('gruby_skul.png');
-            msg.channel.send(attachment);
-        }
-        if (msg.content.toLowerCase() === '!ogromnyskul') 
-        {
-            const attachment = new Discord.MessageAttachment('fat_skul.jpg');
-            msg.channel.send(attachment);
-        }
-        if (msg.content.toLowerCase() === '!kreciolek' | msg.content.toLowerCase() === '!kręciołek') 
-        {
-            const attachment = new Discord.MessageAttachment('skul.gif');
-            msg.channel.send(attachment);
-        }
-        if (msg.content.toLowerCase() === 'skul.') 
-        {
-            (async function doSomeStuff() {
-                const attachment = new Discord.MessageAttachment('gruby_skul.png');
-                for(var i=0;i<20;i++) {
-                    msg.channel.send(attachment);
-                    await new Promise(resolve => setTimeout(resolve, 5000));
-                }
-            })();
-        }
-        if (msg.content.toLowerCase() === 'karol top') 
-        {
-            function getRandomIntInclusive(min, max) {
-                min = Math.ceil(min);
-                max = Math.floor(max);
-                return Math.floor(Math.random() * (max - min + 1)) + min;
-            }
-
-            const losowa = getRandomIntInclusive(0,20000);
-
-            fs.readFile('./api.steampowered.com.json', 'utf8', function (err, data) {
-                if (err) throw err;
-                const obj = JSON.parse(data);
-                const opis = obj.applist.apps[losowa].name;
-                const embed = new Discord.MessageEmbed()
-                .setTitle('Gra, w którą Karol nie zagra: ')
-                .setColor(0x0900AB)
-                .setDescription(opis);
-                
-                msg.channel.send(embed);
-            });
-        }
-        if (msg.content.toLowerCase() === 'karol top sql') 
-        {
-            function getRandomIntInclusive(min, max) {
-                min = Math.ceil(min);
-                max = Math.floor(max);
-                return Math.floor(Math.random() * (max - min + 1)) + min;
-            }
-            const losowa = getRandomIntInclusive(0,113038);
-
-            var mysql = require('mysql');
-            var con = mysql.createConnection({
-                host: "localhost",
-                user: "root",
-                database: "grysteam"
-            });
-
-            con.connect(function(err) {
-                if (err) throw err;
-                con.query('SELECT Nazwa FROM gry WHERE `ID` = ' + losowa + ';', (err, results, fields) => {
-                    if (err) throw err;
-                        const embed = new Discord.MessageEmbed()
-                        .setTitle('Gra, w którą Karol nie zagra: ')
-                        .setColor(0x0900AB)
-                        .setDescription(results[0].Nazwa);
-                        msg.channel.send(embed);
-                });
-            });
-            
-        }
-    });
-    client.login(token);
+          }
+        );
+      });
+    }
   });
+  client.login(token);
+});
